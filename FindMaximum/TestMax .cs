1a7011d1be413ef894aa6a436ce5f102a381ce2d
dabhade904 <dabhade904@gmail.com>
2022-08-10 17:56:25 +0530

    Added : function to find maximum number

---
+++ FindMaximum/TestMax .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class TestMax
+    {
+        public int MaxVal(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int bigNo = firstNumber;
+            if (bigNo > secondNumber)
+            {
+                bigNo = secondNumber;
+            }
+            else if (bigNo > thirdNumber)
+            {
+                bigNo = thirdNumber;
+            }
+            return bigNo;
+        }
+
+    }
+}
